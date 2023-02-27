5eb607299ebf67c5a6ec7552768b1cc2064f7e23
syedDayim <dayim1277@gmail.com>
2023-02-27 22:39:06 +0530

    making Custom Hook useFetch()

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,21 +4,28 @@ const useFetch = (url) => {
     const [data, setdata] = useState(null);    
     const [isPending, setIsPending] = useState(true);  
     const [error, setError] = useState(null);
+
+
     // Fetching blogs from Json database using useEffect()
     useEffect(() => {
-        fetch(url)
-            .then(res => {
-                if(!res.ok){
+        fetch(url) // fetches the blog and returns a promise
+
+            // catching the promise to get the resources
+            .then(res => {  
+                if(!res.ok){ // cheching if the resource is not available and throws an error 
                     throw Error("Couldn't find the resoures")
                 }
-                return res.json();
+                return res.json(); // returns the resource and also a promise
             })
+
+            // catch the promise to get the data 
             .then(data => {
                 setdata(data);
                 setIsPending(false);
                 setError(null);
             })   
 
+            //catching all the errors
             .catch(err => {
                 setError(err.message);
                 setIsPending(false);
@@ -26,7 +33,7 @@ const useFetch = (url) => {
     }, [])
 
 
-    return { data, isPending, error}
+    return { data, isPending, error} // returning the states
 
 }
-export default useFetch;
+export default useFetch; // exporting the custom hook
