5fd95cfce4cc277912294bd54536f459f909f5ac
syedDayim <dayim1277@gmail.com>
2023-03-01 19:58:04 +0530

    Added Form and style

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,48 @@
+import { useState } from "react"
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
     return ( 
         <div className="create">
+            <center>
             <h2>Add a Blog!</h2>
-        </div>
+            </center>
 
+            <form>
+                <label>Title</label>
+                <input
+                    type="text"
+                    required
+                    value={ title }
+                    onChange={ (e) => {setTitle(e.target.value)}  }
+
+                />
+
+                <label>Body</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={ (e)=>{setBody(e.target.value)} }
+                >
+                </textarea>
+
+                <label>Author</label>
+                <input
+                    type="text"
+                    required
+                    value={author}
+                    onChange={ (e)=>{setAuthor(e.target.value)}}
+                />
+
+                <button>Add Blog</button>
+            </form>
+{/* <p>{title} </p>
+<p>{body} </p>
+<p>{author} </p> */}
+
+
+        </div>
 
      );
 }
