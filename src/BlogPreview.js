07b75401e96541b5ce06a5191ce55d970f5728af
syedDayim <dayim1277@gmail.com>
2023-03-01 19:24:39 +0530

    Finished the BlogFetch Page

--- src/BlogPreview.js
+++ src/BlogPreview.js
@@ -1,13 +1,26 @@
 import { useParams } from "react-router";
+import useFetch from "./useFetch";
 
 const BlogPreview = () => {
-    const { id } = useParams();
-    return ( 
-        <div className="blog-preview">
-            <h1> BlogID - { id } </h1>
-        </div>
+  const { id } = useParams();
+  const {
+    data: blog,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs/" + id);
+  return (
+    <div className="blog-details">
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div> {blog.body}</div>
+        </article>
+      )}
+    </div>
+  );
+};
 
-     );
-}
- 
 export default BlogPreview;
